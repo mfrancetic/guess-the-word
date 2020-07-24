fc2a007d458d737c2946b84b63af782c5f7b0118
Maja Francetic <francetic.maja@gmail.com>
2020-07-24 13:10:19 +0200

    Implement a CountdownTimer for 1 minute - when it finished, the game is over

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,10 +17,10 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
+import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -68,6 +68,10 @@ class GameFragment : Fragment() {
 
         viewModel.word.observe(viewLifecycleOwner, Observer { newWord ->
             binding.wordText.text = newWord
+        })
+
+        viewModel.currentTime.observe(viewLifecycleOwner, Observer { mewTime ->
+                binding.timerText.text = DateUtils.formatElapsedTime(mewTime);
         })
 
         viewModel.eventGameFinish.observe(viewLifecycleOwner, Observer { isFinished ->
--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,9 +1,9 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import timber.log.Timber
 
 // extends ViewModel
 class GameViewModel : ViewModel() {
@@ -31,16 +31,51 @@ class GameViewModel : ViewModel() {
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
+    // countdown timer
+    private val _timer: CountDownTimer;
+
+    private val _currentTime = MutableLiveData<Long>();
+
+    val currentTime: LiveData<Long>
+        get() = _currentTime;
 
     init {
         _eventGameFinish.value = false
         resetList()
         nextWord()
         _score.value = 0;
+
+        // creates a timer which triggers the end of the game when it finishes
+        // it will tick every ONE_SECOND until the COUNTDOWN_TIME passes
+        _timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+            override fun onTick(milisUntilFinished: Long) {
+                _currentTime.value = milisUntilFinished / ONE_SECOND
+            }
+
+            override fun onFinish() {
+                _eventGameFinish.value = true;
+                _currentTime.value = DONE
+            }
+        }
+        _timer.start();
+    }
+
+    companion object {
+        // these represent different important times in the game, such as game length
+
+        // this is when the game is over
+        private const val DONE = 0L;
+
+        // this is the number of miliseconds in a second
+        private const val ONE_SECOND = 1000L;
+
+        // this is the total time of the game
+        private const val COUNTDOWN_TIME = 60000L;
     }
 
     override fun onCleared() {
         super.onCleared()
+        _timer.cancel();
     }
 
     /**
@@ -79,11 +114,9 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         //Select and remove a word from the list
         if (wordList.isEmpty()) {
-            // game has finished
-            _eventGameFinish.value = true
-        } else {
-            _word.value = wordList.removeAt(0)
+            resetList()
         }
+        _word.value = wordList.removeAt(0)
     }
 
 
